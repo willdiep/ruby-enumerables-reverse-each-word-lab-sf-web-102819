1f0823811309c04c81155e92401f8493efd9937c
Will Diep <widiep@gmail.com>
2019-09-27 09:54:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,16 @@
+# def reverse_each_word(str)
+# 	str_To_arr = str.split
+# 	# str_To_arr
+# 	result = []
+# 	str_To_arr.each do |word|
+# 		result.push(word.reverse)
+# 	end
+# 	result.join(" ")
+# end
+
 def reverse_each_word(str)
 	str_To_arr = str.split
-	# str_To_arr
-	result = []
-	str_To_arr.each do |word|
-		result.push(word.reverse)
+	str_To_arr.collect do |word|
+		word.reverse
 	end
-	result.join(" ")
 end
